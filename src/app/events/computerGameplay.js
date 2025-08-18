36e26b85946c12844721ca6589f26e41e616a8f4
zakinotabi <101570337+zakno52@users.noreply.github.com>
2025-08-18 20:56:11 +0100

    try improve computer

--- src/app/events/computerGameplay.js
+++ src/app/events/computerGameplay.js
@@ -1,5 +1,7 @@
 import attack from '../gameAction/attack';
 import gameState from '../init/initializePlayers';
+let lastHit;
+let skipCounter;
 
 export default function computerPlay(coord) {
   const comp = gameState.players.comp;
@@ -8,13 +10,59 @@ export default function computerPlay(coord) {
   const cell = activeBoard.querySelector(`[data-index="${attackCoord}"]`);
 
   //   if (!cell) return;
-  if (!isNaN(comp.op.gameboard[attackCoord])) {
-    attack(cell, comp.op);
-    attackCoord === 99 ? (attackCoord -= 1) : attackCoord;
-    computerPlay(attackCoord + 1);
-  } else if (comp.op.gameboard[attackCoord] === 'hit' || comp.op.gameboard[attackCoord] === 'miss') {
-    computerPlay();
-  } else if (comp.op.gameboard[attackCoord] === '.') {
-    attack(cell, comp.op);
+  if (lastHit) {
+    if (attackCoord === lastHit + 1) {
+      return play(attackCoord + 1);
+      // return computerPlay(attackCoord + 1);
+    }
+    if (attackCoord === lastHit - 1) {
+      return play(attackCoord - 1);
+      // return computerPlay(attackCoord - 1);
+    }
+    if (attackCoord === lastHit + 10) {
+      return play(attackCoord + 10);
+      // return computerPlay(attackCoord + 10);
+    }
+    if (attackCoord === lastHit - 10) {
+      return play(attackCoord - 10);
+      // return computerPlay(attackCoord - 10);
+    }
+  } else {
+    skipCounter = 0;
+    play(attackCoord);
+  }
+
+  function play(coords) {
+    if (!isNaN(comp.op.gameboard[coords])) {
+      lastHit = coords;
+      // if (skipCounter === 0) {
+      //   skipCounter += 1;
+      //   return computerPlay(coords + 1);
+      // }
+      // if (skipCounter === 1) {
+      //   skipCounter += 1;
+      //   return computerPlay(coords - 1);
+      // }
+      // if (skipCounter === 2) {
+      //   skipCounter += 1;
+      //   return computerPlay(coords + 10);
+      // }
+      // if (skipCounter === 3) {
+      //   skipCounter += 1;
+      //   return computerPlay(attackCoord - 10);
+      // }
+      setTimeout(() => {
+        attack(cell, comp.op);
+        coords === 99 ? (coords -= 1) : coords;
+        computerPlay(coords + 1);
+      }, 500);
+    } else if (comp.op.gameboard[coords] === 'hit' || comp.op.gameboard[coords] === 'miss') {
+      computerPlay();
+    } else if (comp.op.gameboard[coords] === '.') {
+      setTimeout(() => {
+        attack(cell, comp.op);
+      }, 500);
+      // attack(cell, comp.op);
+    }
   }
 }
